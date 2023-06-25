6c04d817d1116314e11d768b164c403243d79ede
Jamie Taylor <Jamie.Taylor@pobox.com>
2023-06-25 12:19:33 -0400

    Add dynamic paragraph option.
    Update manifest version to 1.6, min SMAPI version to 3.17

---
+++ StardewGMCMOptions/Framework/GameTextLayoutEngine.cs
@@ -0,0 +1,26 @@
+﻿// Copyright 2023 Jamie Taylor
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewValley;
+
+namespace GMCMOptions.Framework {
+    public class GameTextLayoutEngine : ITextLayoutEngine {
+        private string formattedText = "";
+        public GameTextLayoutEngine() {
+        }
+
+        /// <inheritdoc/>
+        public int Layout(string text, int width) {
+            formattedText = Game1.parseText(text, Game1.smallFont, width);
+            return (int)Game1.smallFont.MeasureString(formattedText).Y;
+        }
+
+        /// <inheritdoc/>
+        public void DrawLastLayout(SpriteBatch b, int left, int top) {
+            b.DrawString(Game1.smallFont, formattedText, new Vector2(left, top), Color.Black);
+        }
+
+    }
+}
+
---
+++ StardewGMCMOptions/Framework/ITextLayoutEngine.cs
@@ -0,0 +1,24 @@
+﻿// Copyright 2023 Jamie Taylor
+using System;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GMCMOptions.Framework {
+    public interface ITextLayoutEngine {
+        /// <summary>
+        ///   Compute the layout for the given text and wrap width, returning the total height used.
+        /// </summary>
+        /// <param name="text">The text to lay out.</param>
+        /// <param name="width">The wrap width.</param>
+        /// <returns>The height used by the layout.</returns>
+        int Layout(string text, int width);
+
+        /// <summary>
+        ///   Draw the layout computed in the most recent call to <c cref="Layout(string, int)">Layout</c>.
+        /// </summary>
+        /// <param name="b">The SpriteBatch in which to draw.</param>
+        /// <param name="left">The left pixel coordinate of the draw position.</param>
+        /// <param name="top">The top pixel coordinate of the draw position.</param>
+        void DrawLastLayout(SpriteBatch b, int left, int top);
+    }
+}
+
